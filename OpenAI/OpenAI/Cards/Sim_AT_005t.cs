5cf6712f66972ab9e5c0f9910d57e41d59632cd9
Martijn Vegter <mvegter.coding@gmail.com>
2017-05-13 22:41:34 +0200

    Restored Cards

---
+++ OpenAI/OpenAI/Cards/Sim_AT_005t.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenAI
+{
+    class Sim_AT_005t : SimTemplate// Boar
+    {
+
+        //Charge
+
+       
+
+    }
+}
